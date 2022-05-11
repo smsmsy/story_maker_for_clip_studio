12a2b6fead04862817e27248a0d1e995654ac28b
smsmsy <smsmsy.dev@gmail.com>
2022-05-12 02:26:47 +0900

    add TextEditingController and fix dart data class

--- lib/classes/content_class.dart
+++ lib/classes/content_class.dart
@@ -1,47 +1,65 @@
+import 'package:flutter/cupertino.dart';
+
 import 'person_class.dart';
 
 /// 単純なContentクラス
 class Content {
   Person person;
   String line;
+  TextEditingController controller;
 
 //<editor-fold desc="Data Methods">
+
 
   Content({
     required this.person,
     required this.line,
+    required this.controller,
   });
+
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      (other is Content &&
-          runtimeType == other.runtimeType &&
-          person == other.person &&
-          line == other.line);
+          (other is Content &&
+              runtimeType == other.runtimeType &&
+              person == other.person &&
+              line == other.line &&
+              controller == other.controller
+          );
+
 
   @override
-  int get hashCode => person.hashCode ^ line.hashCode;
+  int get hashCode =>
+      person.hashCode ^
+      line.hashCode ^
+      controller.hashCode;
+
 
   @override
   String toString() {
-    return 'Content{' + ' person: $person,' + ' line: $line,' + '}';
+    return 'Content{ person: $person, line: $line, controller: $controller }';
   }
+
 
   Content copyWith({
     Person? person,
     String? line,
+    TextEditingController? controller,
   }) {
     return Content(
       person: person ?? this.person,
       line: line ?? this.line,
+      controller: controller ?? this.controller,
     );
   }
 
+
   Map<String, dynamic> toMap() {
     return {
-      'person': this.person,
-      'line': this.line,
+      'person': person,
+      'line': line,
+      'controller': controller,
     };
   }
 
@@ -49,8 +67,10 @@ class Content {
     return Content(
       person: map['person'] as Person,
       line: map['line'] as String,
+      controller: map['controller'] as TextEditingController,
     );
   }
+
 
 //</editor-fold>
 }
